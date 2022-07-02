a31e3c8f767b744e6fd161cb0a7e7aed9ab70abc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-02 11:22:33 +0530

    blit the image

--- runner.py
+++ runner.py
@@ -11,6 +11,7 @@ White =( 255,255,255)
 window = pygame.display.set_mode((width,height))
 right_g = pygame.image.load(os.path.join('Assets','tiles','1.png'))
 while True:
+    window.blit(right_g,(100,100))
     window.fill(Black)
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
