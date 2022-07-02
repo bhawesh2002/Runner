67db1d788d270391a425e13467519b42ef739a62
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-02 18:38:14 +0530

    blit ground on window

--- runner.py
+++ runner.py
@@ -7,13 +7,13 @@ pygame.init()
 
 width,height = 800,600
 Black = (0,0,0)
-White =( 255,255,255)
+White =(255,255,255)
 window = pygame.display.set_mode((width,height))
-right_g = pygame.image.load(os.path.join('Assets','tiles','1.png'))
+ground = pygame.image.load(os.path.join('Assets','tiles','2.png'))
 while True:
     pygame.display.update()
     window.fill(Black)
-    window.blit(right_g,(500,500))
+    window.blit(ground,(0,500))
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
