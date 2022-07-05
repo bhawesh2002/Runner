feb4c4f3e2f096e1b7eb989f27727a692ff36e52
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-07-05 22:59:11 +0530

    reset x_pos_g to 0

--- runner.py
+++ runner.py
@@ -17,7 +17,7 @@ grass = pygame.image.load(os.path.join('Assets','tiles','7.png'))
 chris = pygame.image.load(os.path.join('Assets','character','chris standing.png'))
 chris_scaled = pygame.transform.scale(chris,(90,130))
 pixels = 128
-x_pos = 0
+x_pos_g = 0
 while True:
     pygame.display.update()
     window.blit(chris_scaled,(150,370))
@@ -25,8 +25,10 @@ while True:
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
             sys.exit()
-    while(x_pos < 800):
-        window.blit(ground, (x_pos, 500))
-        x_pos += pixels
+    while(x_pos_g < 800):
+        window.blit(ground, (x_pos_g, 500))
+        x_pos_g += pixels
+        if(x_pos_g > 800):
+            x_pos_g = 0
+            break
         pygame.display.update()
-    x_pos = 0
